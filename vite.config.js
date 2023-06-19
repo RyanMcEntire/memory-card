1555bac82d30655847f0997eb27d9ff37e9edd68
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2023-06-18 23:31:27 -0600

    unblock dist again

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Memory-Card/',
+  base: '/memory-card/',
   plugins: [react()],
 });
